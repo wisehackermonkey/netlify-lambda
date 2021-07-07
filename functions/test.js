5ed083aebbe571a113e29f504801fd4c496f9299
Oran C <Oranbusiness@gmail.com>
2021-07-07 16:25:21 -0700

    [test]

--- functions/test.js
+++ functions/test.js
@@ -1,6 +1,7 @@
 exports.handler = (event, context, callback) => {
     callback(null, {
         statusCode: 200,
-        body: "No worries, all is working fine!"
+        body: "No worries, all is working fine!" +
+         `${event.path},${event.httpMethod},${event.headers},${event.queryStringParameters},${event.body},`
     })
 }
