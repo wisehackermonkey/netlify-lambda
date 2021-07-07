199de750771cc4ec169b8269738196b1ff8408c7
Oran C <Oranbusiness@gmail.com>
2021-07-07 16:40:42 -0700

    [add] env's

--- functions/test.js
+++ functions/test.js
@@ -1,9 +1,11 @@
+const { SECRET_API } = process.env 
+
 // non async
 exports.handler = (event, context, callback) => {
     callback(null, {
         statusCode: 200,
         body: "No worries, all is working fine!" +
-         `${event.path},${event.httpMethod},${event.headers},${JSON.stringify(event.queryStringParameters)},${JSON.stringify(event.body)},`
+            `${event.path},${event.httpMethod},${event.headers},${JSON.stringify(event.queryStringParameters)},${JSON.stringify(event.body)},`
     })
 }
 
@@ -19,8 +21,22 @@ exports.handler = (event, context, callback) => {
 // 
 exports.handler = async (event, context) => {
     const name = event.queryStringParameters.name || "banana"
-    return {
-        statusCode: 200,
-        body: `Wow my api works! Welcome ${name}`
+
+    if (event.httpMethod === "POST") {
+
+        // When the method is POST, the name will no longer be in the event’s
+        // queryStringParameters – it’ll be in the event body encoded as a query string
+        const params = querystring.parse(event.body);
+        const name = params.name || "World";
+
+        return {
+            statusCode: 200,
+            body: `Hello, ${name}`,
+        };
+    } else {
+        return {
+            statusCode: 200,
+            body: `Wow my api works! Welcome ${name} ${SECRET_API}`
+        }
     }
 }
