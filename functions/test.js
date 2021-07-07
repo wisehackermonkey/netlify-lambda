7bea1c10a06cb5a711b375a5e7bd7f8f375058ce
Oran C <Oranbusiness@gmail.com>
2021-07-07 16:42:28 -0700

    [fix] uncommented out function handler

--- functions/test.js
+++ functions/test.js
@@ -1,13 +1,13 @@
 const { SECRET_API } = process.env 
 
 // non async
-exports.handler = (event, context, callback) => {
-    callback(null, {
-        statusCode: 200,
-        body: "No worries, all is working fine!" +
-            `${event.path},${event.httpMethod},${event.headers},${JSON.stringify(event.queryStringParameters)},${JSON.stringify(event.body)},`
-    })
-}
+// exports.handler = (event, context, callback) => {
+//     callback(null, {
+//         statusCode: 200,
+//         body: "No worries, all is working fine!" +
+//             `${event.path},${event.httpMethod},${event.headers},${JSON.stringify(event.queryStringParameters)},${JSON.stringify(event.body)},`
+//     })
+// }
 
 // aync version
 
