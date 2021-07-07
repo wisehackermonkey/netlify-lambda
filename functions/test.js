e9885b336ad5f09bfaedf6a3b49a18b67af8d8b3
Oran C <Oranbusiness@gmail.com>
2021-07-07 16:26:23 -0700

    [test] 2

--- functions/test.js
+++ functions/test.js
@@ -2,6 +2,6 @@ exports.handler = (event, context, callback) => {
     callback(null, {
         statusCode: 200,
         body: "No worries, all is working fine!" +
-         `${event.path},${event.httpMethod},${event.headers},${event.queryStringParameters},${event.body},`
+         `${event.path},${event.httpMethod},${event.headers},${JSON.stringify(event.queryStringParameters)},${JSON.stringify(event.body)},`
     })
 }
