6fab3291b0a8e707e7d3b0be1982c78f0d7dfb76
Oran C <Oranbusiness@gmail.com>
2021-07-07 16:34:52 -0700

    [update] to async

--- functions/test.js
+++ functions/test.js
@@ -1,7 +1,17 @@
+// non async
 exports.handler = (event, context, callback) => {
     callback(null, {
         statusCode: 200,
         body: "No worries, all is working fine!" +
          `${event.path},${event.httpMethod},${event.headers},${JSON.stringify(event.queryStringParameters)},${JSON.stringify(event.body)},`
     })
+}
+
+// aync version
+
+exports.handler = async (event, context) => {
+    return {
+        statusCode: 200,
+        body: "Wow my api works!"
+    }
 }
